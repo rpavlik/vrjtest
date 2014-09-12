0535f94adef3b2bc7beee1a5eabb48647605b622
Ryan Pavlik <rpavlik@iastate.edu>
2014-09-12 13:01:10 -0500

    Commit stubs.

---
+++ src/GetSurfaces.h
@@ -0,0 +1,31 @@
+/** @file
+	@brief Header
+
+	@date 2014
+
+	@author
+	Ryan Pavlik
+	<rpavlik@iastate.edu> and <abiryan@ryand.net>
+	http://academic.cleardefinition.com/
+	Iowa State University Virtual Reality Applications Center
+	Human-Computer Interaction Graduate Program
+*/
+
+//          Copyright Iowa State University 2014.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef INCLUDED_GetSurfaces_h_GUID_03fce17e_6d3b_4365_9a99_0f7de690b9dd
+#define INCLUDED_GetSurfaces_h_GUID_03fce17e_6d3b_4365_9a99_0f7de690b9dd
+
+// Internal Includes
+// - none
+
+// Library/third-party includes
+// - none
+
+// Standard includes
+// - none
+
+#endif // INCLUDED_GetSurfaces_h_GUID_03fce17e_6d3b_4365_9a99_0f7de690b9dd
---
+++ src/vrjtest.cpp
@@ -0,0 +1,28 @@
+/**
+	@file
+	@brief Main file - entry point.
+
+	@date 2014
+
+	@author
+	Ryan Pavlik
+	<rpavlik@iastate.edu> and <abiryan@ryand.net>
+	http://academic.cleardefinition.com/
+	Iowa State University Virtual Reality Applications Center
+	Human-Computer Interaction Graduate Program
+*/
+
+//          Copyright Iowa State University 2014.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+// Internal Includes
+// - none
+
+// Library/third-party includes
+// - none
+
+// Standard includes
+// - none
+
